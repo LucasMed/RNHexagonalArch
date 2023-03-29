554a4158cda9cbe1a337e43e89e50c72274e2298
Medina Lucas <medinalucas.j@gmail.com>
2023-03-29 19:35:26 -0300

    Se instalan la librerías. Se configura eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'eslint:recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/sort-styles': [
+      'error',
+      'asc',
+      {
+        ignoreClassNames: false,
+        ignoreStyleProperties: false,
+      },
+    ],
+  },
 };
